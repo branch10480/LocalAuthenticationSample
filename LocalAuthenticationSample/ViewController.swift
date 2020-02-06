aaf63beab9f092bdeac647422fcf3b9e8cdc1c23
ixit-toshi-imaeda <toshi-imaeda@ixit.co.jp>
2020-02-06 21:44:21 +0900

    Initial Commit

---
+++ LocalAuthenticationSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LocalAuthenticationSample
+//
+//  Created by 今枝 稔晴 on 2020/02/06.
+//  Copyright © 2020 今枝 稔晴. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
